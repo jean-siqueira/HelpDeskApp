03ed6ecb6eb31d4b4beabc06a19a0c4da03ce0c3
jean-siqueira <jean.siqueira@outlook.com.br>
2019-04-20 19:55:59 -0300

    finalização

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { SummaryComponent } from './components/summary/summary.component';
 import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.component';
 import { TicketListComponent } from './components/ticket-list/ticket-list.component';
 import { TicketNewComponent } from './components/ticket-new/ticket-new.component';
@@ -18,7 +19,8 @@ export const ROUTES: Routes = [
     { path : 'ticket-list', component : TicketListComponent, canActivate: [AuthGuard] },
     { path : 'ticket-new', component : TicketNewComponent, canActivate: [AuthGuard] },
     { path : 'tikcet-new/:id', component : TicketNewComponent, canActivate: [AuthGuard] },
-    { path : 'ticket-detail/:id', component : TicketDetailComponent, canActivate: [AuthGuard] }
+    { path : 'ticket-detail/:id', component : TicketDetailComponent, canActivate: [AuthGuard] },
+    { path : 'summary', component : SummaryComponent, canActivate: [AuthGuard] }
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
